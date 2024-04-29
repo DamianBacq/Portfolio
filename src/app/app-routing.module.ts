18ae5221b92830d4b429f2c99d8cf99886022641
DamianBacq <damian.bacque235@gmail.com>
2024-04-29 18:35:18 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { ProyectosComponent } from './Pages/proyectos/proyectos.component';
+import { ContactoComponent } from './Pages/contacto/contacto.component';
+import { FormacionComponent } from './Pages/formacion/formacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'formacion', component: FormacionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ProyectosComponent } from './Pages/proyectos/proyectos.component';
+import { ContactoComponent } from './Pages/contacto/contacto.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormacionComponent } from './Pages/formacion/formacion.component';
+import { HttpClientModule } from '@angular/common/http';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AboutComponent,
+    NavbarComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    FormacionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    MatGridListModule,
+    MatCardModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+
+}
