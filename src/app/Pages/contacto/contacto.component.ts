30bb85e8b4bce0a1a40591942a080d611743592a
DamianBacq <damian.bacque235@gmail.com>
2024-05-03 18:08:06 -0300

    Solucionando cosas del contaco

--- src/app/Pages/contacto/contacto.component.ts
+++ src/app/Pages/contacto/contacto.component.ts
@@ -4,26 +4,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrl: './contacto.component.css',
 })
 export class ContactoComponent {
-
-  myForm: FormGroup
-  constructor(private fb:FormBuilder){
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
-      nombre:[],
+      nombre: [],
       asunto: [],
       email: [],
-      mensaje: []
-    })
+      mensaje: [],
+    });
   }
 
-  Enviar(){
+  Enviar() {
     const formData = this.myForm.value;
 
-    const subject = formData.asunto;
-    const body = `Nombre: ${formData.nombre}\nEmail: ${formData.email}\nMensaje: ${formData.mensaje}`;
+    const subject = encodeURIComponent(formData.asunto);
+    const body = 
+    `Nombre: ${formData.nombre}\n`+
+    `Email: ${formData.email}\n` +
+    `Mensaje: ${formData.mensaje}`;
 
-    window.location.href=`mailto:damian.bacque235@gmail.com?subject=${subject}&body=${body}`;
+    window.location.href = `mailto:damian.bacque235@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
   }
 }
